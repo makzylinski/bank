e8df94af5cc04f5b87b86960964332d19dec402c
Maks <makzylinski@gmail.com>
2024-08-05 08:22:56 +0200

    fixes

--- src/Bank.java
+++ src/Bank.java
@@ -15,53 +15,57 @@ public class Bank {
         }
 
         branches.add(new Branch(branchName));
+        System.out.println("Branches " + branches);
         System.out.println("New Branch has been added, name " + branchName);
         return true;
     }
 
     public boolean addCustomer(String branchName, String customerName, double initialTransaction) {
-        if(findBranch(branchName) == null) {
-            return false;
-        }
-
         Branch branch = findBranch(branchName);
-        branch.newCustomer(customerName, initialTransaction);
-        return true;
+        if (branch != null) {
+            return branch.newCustomer(customerName, initialTransaction);
+        }
+        return false;
     }
 
     public boolean addCustomerTransaction(String branchName, String customerName, double transaction) {
-        if(findBranch(branchName) == null) {
-            return false;
-        }
-
         Branch branch = findBranch(branchName);
-        branch.addCustomerTransaction(customerName, transaction);
-        return true;
+        if (branch != null) {
+            return branch.addCustomerTransaction(customerName, transaction);
+        }
+        return false;
     }
 
     private Branch findBranch(String branchName) {
-        for (int i = 0; i < branches.size() - 1; i++) {
+
+        for (int i = 0; i < branches.size(); i++) {
             if(branches.get(i).getName().equalsIgnoreCase(branchName)) {
                 return branches.get(i);
             }
         }
-
         return null;
     }
 
-    public boolean listCustomers(String branchName, boolean printTransactions) {
-        System.out.println(branches);
-        if(findBranch(branchName) != null) {
-            return false;
+    public boolean listCustomers(String branchName, boolean showTransactions) {
+
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            System.out.println("Customer details for branch " + branch.getName());
+            ArrayList<Customer> branchCustomers = branch.getCustomers();
+            for (int i = 0; i < branchCustomers.size(); i++) {
+                Customer branchCustomer = branchCustomers.get(i);
+                System.out.println("Customer: " + branchCustomer.getName() + "[" + (i+1) + "]");
+                if (showTransactions) {
+                    System.out.println("Transactions");
+                    ArrayList<Double> transactions = branchCustomer.getTransactions();
+                    for (int j = 0; j < transactions.size(); j++) {
+                        System.out.println("[" + (j+1) + "] Amount " + transactions.get(j));
+                    }
+                }
+            }
+            return true;
         }
-        System.out.println(branches);
-        if(printTransactions) {
-
-
-            System.out.println("Customer details for branch " + branchName);
-//            System.out.println("Customer: " + );
-        }
-
         return false;
     }
+
 }
--- src/Branch.java
+++ src/Branch.java
@@ -18,34 +18,37 @@ public class Branch {
     }
 
     public boolean newCustomer(String name, double initTransaction) {
-        if(findCustomer(name) != null) {
-            return false;
+        if (findCustomer(name) == null) {
+            customers.add(new Customer(name, initTransaction));
+            return true;
         }
-        new Customer(name, initTransaction);
-        return true;
+        return false;
     }
 
     public boolean addCustomerTransaction(String name, double transaction) {
-        if(findCustomer(name) == null) {
-            return false;
+        Customer existingCustomer = findCustomer(name);
+        if (existingCustomer != null) {
+            existingCustomer.addTransaction(transaction);
+            return true;
         }
-
-        if(Double.valueOf(transaction) != null) {
-            return false;
-        }
-
-        findCustomer(name).addTransaction(transaction);
-        return true;
+        return false;
     }
 
 
     private Customer findCustomer(String name) {
-        for (int i = 0; i < customers.size() - 1; i++) {
+        for (int i = 0; i < customers.size(); i++) {
             if(customers.get(i).getName().equalsIgnoreCase(name)) {
                 return customers.get(i);
             }
         }
 
         return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Branch{" +
+                "customers=" + customers +
+                '}';
     }
 }
